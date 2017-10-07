4704a54b856d1b91d4254dc5358772d22ee66e12
ajpershick <ajpershick@gmail.com>
2017-10-06 18:17:25 -0700

    finish e4 except for answers.txt

--- e4/temperature_correlation.py
+++ e4/temperature_correlation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sys
 import gzip
 import numpy as np
+import matplotlib.pyplot as plt
 
 stations = sys.argv[1]
 city_data = sys.argv[2]
@@ -13,7 +14,8 @@ stations['avg_tmax'] = stations['avg_tmax']/10
 city_data = pd.read_csv(city_data)
 city_data = city_data.dropna()
 city_data['area'] = city_data['area']/1000000
-city_data = city_data[city_data['area'] <= 10000]
+city_data = city_data[city_data['area'] <= 10000].reset_index()
+city_data = city_data.drop('index', 1)
 
 # Reusing from e3
 def haversine_np(lon1, lat1, lon2, lat2):
@@ -32,13 +34,19 @@ def haversine_np(lon1, lat1, lon2, lat2):
 def distance(city, stations):
     dist = haversine_np(city['longitude'], city['latitude'],
                         stations['longitude'], stations['latitude'])
-    dist = np.sum(dist)
     return dist
 
 def best_tmax(city, stations):
-    closest_city = distance(city, stations).argmin()
-    return closest_city
+    dist = distance(city, stations)
+    return stations.get_value(index=dist.argmin(), col='avg_tmax')
 
-closest = best_tmax(city_data, stations )
+Calgary = city_data.iloc[0]
+calgTmax = best_tmax(Calgary, stations)
+city_data['best_tmax'] = city_data.apply(best_tmax, stations=stations, axis=1)
+city_data['population_density'] = city_data['population']/city_data['area']
 
-s
+plt.plot(city_data['best_tmax'], city_data['population_density'], 'b.', alpha=0.5)
+plt.ylabel('Population Density (people/km\u00b2)')
+plt.xlabel('Avg Max Temperature (\u00b0C)')
+plt.savefig(output)
+
