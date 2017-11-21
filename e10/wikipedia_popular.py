3385fe4657e60c1071e6b9a2f9433f79bef053cc
ajpershick <ajpershick@gmail.com>
2017-11-20 16:38:11 -0800

    finish Wiki

--- e10/wikipedia_popular.py
+++ e10/wikipedia_popular.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from pyspark.sql import SparkSession, functions, types
 
 spark = SparkSession.builder.appName('wikipedia popular').getOrCreate()
@@ -9,18 +10,48 @@ assert spark.version >= '2.2' # make sure we have Spark 2.2+
 schema = types.StructType([  # commented-out fields won't be read
     types.StructField('lang', types.StringType(), False),
     types.StructField('name', types.StringType(), False),
-    types.StructField('reqs', types.LongType(), False),
     types.StructField('views', types.LongType(), False),
+    types.StructField('bytes', types.LongType(), False),
 ])
+
+# https://stackoverflow.com/questions/678236/how-to-get-the-filename-without-the-extension-from-a-path-in-python
+def convert_pathname(path):
+    return os.path.splitext(path)[0][-15:-4]
 
 def main(in_directory, out_directory):
     data = spark.read.csv(in_directory, sep=' ', schema=schema).withColumn('filename', functions.input_file_name())
+    path_to_hour = functions.udf(convert_pathname, returnType = types.StringType())
+    data = data.filter(data['lang'] == 'en')
+    data = data.filter(data['name'] != 'Main_Page')
+    data = data.filter(~data['name'].startswith('Special:'))
+
+    data = data.select(
+        data['lang'],
+        data['name'],
+        data['views'],
+        data['bytes'],
+        path_to_hour(data['filename']).alias("hour")
+    )
+
+    groupby_hour = data.groupBy("hour").agg(functions.max("views").alias('views'))
     
+    groupby_hour = groupby_hour.select(
+        groupby_hour['hour'],
+        groupby_hour['views']
+    )
+        
+    data = data.join(groupby_hour, ['hour', 'views'])
+    data = data.orderBy('hour')
+    data = data.select(
+        data['hour'],
+        data['name'],
+        data['views']
+    )
+    #data.show(100)
 
-
+    data.write.csv(out_directory + '-wiki', mode='overwrite')
 
 if __name__=='__main__':
     in_directory = sys.argv[1]
     out_directory = sys.argv[2]
-    main(in_directory, out_directory)
     main(in_directory, out_directory)
